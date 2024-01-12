d40dbba23b3d408e4ad988265f985b1137129942
vardhan1831 <sreevardhan259@gmail.com>
2024-01-12 11:53:55 +0530

    Update ExpenseTracker component with logout button.

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -1,44 +1,53 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Button, Navbar} from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-const ExpenseTracker = ()=>{
-    const verificationHandler = async () =>{
-        try{
-            const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${process.env.REACT_APP_AUTH_KEY}`,{
-                method:'POST',
-                body:JSON.stringify({
-                    requestType:"VERIFY_EMAIL",
-                    idToken:localStorage.getItem('token')
-                }),
-                headers:{
-                    'Content-type':'application/json; charset=UTF-8'
-                }
-            })
-            const data = await res.json()
-            if(!res.ok){
-                throw new Error(data.error.message || 'Something went wrong!');
-            }else{
-                console.log(data)
-                alert('please check your email to verify')
-            }
-        }catch(err){
-            console.log(err.message)
+const ExpenseTracker = () => {
+  const history = useHistory();
+  const verificationHandler = async () => {
+    try {
+      const res = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${process.env.REACT_APP_AUTH_KEY}`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken: localStorage.getItem("token"),
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
         }
+      );
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data.error.message || "Something went wrong!");
+      } else {
+        console.log(data);
+        // localStorage.setItem("isVerified", true)
+        alert("please check your email to verify");
+      }
+    } catch (err) {
+      console.log(err.message);
     }
-    return (
-        <>
-        <header>
-            <nav style={{display:'flex',justifyContent:'space-between'}}> 
-            <h1>Welcome to Expense Tracker !!</h1>
-            <Button onClick={verificationHandler}>Verify email</Button>
-            <div>
-                Your profile is incomplete, <Link to='/profile'>click here</Link> to update your profile
-            </div>
-            </nav>
-        </header>
-        </>
-    )
-}
+  };
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    history.replace("/login");
+  };
+  return (
+    <Navbar className="bg-body-tertiary" style={{height:'4rem',display:'flex',justifyContent:'space-between'}}>
+        <Navbar.Brand style={{fontWeight:'800',marginLeft:'2rem'}}>Expense Tracker</Navbar.Brand>
+        <div style={{display:'flex',justifyContent:'space-between'}}>
+        <Button onClick={verificationHandler}>Verify email</Button>
+          <Button onClick={logoutHandler}>Logout</Button>
+          <Navbar.Text>
+            <Link to="/profile">click here</Link> to update your profile.
+          </Navbar.Text>
+        </div>
+          
+    </Navbar>
+  );
+};
 
-export default ExpenseTracker
+export default ExpenseTracker;
